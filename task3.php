54f413ab975ebc68d2f74c5a4862de95ea4e5608
ZakiaMim <84720090+ZakiaMim@users.noreply.github.com>
2021-06-26 19:28:02 +0600

    upload
    upload

---
+++ task3.php
@@ -0,0 +1,85 @@
+<?php
+$validatename="";
+$validateemail="";
+$validatepass="";
+$validatecheckbox="";
+$validateradio="";
+$h1=$h2=$h3="";
+$name=$email=$gender=$hobby="";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$name=$_REQUEST["fname"];
+$email=$_REQUEST["email"];
+$password=$_REQUEST["password"];
+if(empty($_REQUEST["fname"]) || (strlen($_REQUEST["fname"])<3))
+{
+    $validatename= "[!! you must enter your name!!] ";
+
+}
+else
+{
+    $name=$_REQUEST["fname"];
+}
+
+if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email))
+{
+    $validateemail="[!! you must enter valid email!!]";
+}
+else{
+    $validateemail= "your email is ".$email;
+}
+if(empty($_REQUEST["password"]) || (strlen($_REQUEST["password"])<3))
+{
+    $validatepass= "[!! you must enter password!!]";
+
+}
+else
+{
+    $password=$_REQUEST["password"];
+}
+
+
+if(!isset($_REQUEST["hobby1"])&&!isset($_REQUEST["hobby2"])&&!isset($_REQUEST["hobby3"]))
+{
+    $validatecheckbox = "[!! please choose your hobby!!]";
+    
+}
+else{
+   if(isset($_REQUEST["hobby1"]))
+   {
+        $h1= $_REQUEST["hobby1"];
+   }
+   if(isset($_REQUEST["hobby2"]))
+   { 
+        $h2= $_REQUEST["hobby2"];
+   }
+   if(isset($_REQUEST["hobby3"]))
+   {
+        $h3= $_REQUEST["hobby3"];
+   }
+ }
+ if(isset($_REQUEST["gender"]))
+{
+    $validateradio= $_REQUEST["gender"];
+}
+else{
+    $validateradio= "[!! please choose your gender!!]";
+}
+$target_file="files/".$_FILES["fileupload"]["name"];
+if (move_uploaded_file($_FILES["fileupload"]["tmp_name"],$target_file))
+   {
+       echo "you have uploaded ".$_FILES["fileupload"]["name"];
+       echo  "<br>". $_FILES["fileupload"]["type"];
+       echo  "<img src=" .$target_file.">";
+
+   }
+   else{
+       echo "sorry , there was an error";
+   }
+
+
+}
+
+
+
+?>
